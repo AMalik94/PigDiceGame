fc2fb65881b578553d6335fbf6fe41e0bb423b1e
Abdullah Malik <abdullah.malik94@gmail.com>
2019-04-23 10:54:46 +0100

    added dice function

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var scores = [0,0];
 var roundScore = 0;
 var activePlayer = 0;
 
-var dice = Math.floor(Math.random() * 7);
+document.querySelector('#score-0').textContent = '0'
 
-document.querySelector("current-0").textContent = dice
+var x = document.querySelector("#score-0").textContent
+
+document.querySelector('.dice').style.display = 'none'
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');
+
+    diceDOM.style.display = 'block';
+    diceDOM.src ='dice-' + dice + '.png'
+
+
+
+})
