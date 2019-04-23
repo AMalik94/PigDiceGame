b8b844140d6a0aacfa12ec24b58f685bb8db45a7
Abdullah Malik <abdullah.malik94@gmail.com>
2019-04-23 09:58:53 +0100

    added dom element

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ var scores = [0,0];
 var roundScore = 0;
 var activePlayer = 0;
 
-var dice = Math.floor(Math.random() * 7)
+var dice = Math.floor(Math.random() * 7);
 
+document.querySelector("current-0").textContent = dice
